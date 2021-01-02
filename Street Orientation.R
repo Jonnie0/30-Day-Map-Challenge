81a27ed6b632edf5626235dd9880cf063427848f
Jonnie0 <jonathan.kabel@gmail.com>
2021-01-02 12:58:20 -0800

    Added parallel lapply to speed up

--- Street Orientation.R
+++ Street Orientation.R
@@ -4,6 +4,7 @@ library(sf)
 library(geosphere)
 library(showtext)
 library(patchwork)
+library(parallel)
 
 font_add_google('Bebas Neue')
 showtext_auto(enable = TRUE)
@@ -18,25 +19,26 @@ get_streets_ind <- function(location, bin) {
   streets <- bind_rows(streets, tibble(bearing = streets$bearing - 180, length = streets$length)) %>% 
     mutate(bearing = (round(bearing/bin, 0) * bin) %% 360 - (bin/2))
   plot <- ggplot_build(ggplot(data = streets, mapping = aes(x = bearing, weight = length)) +
-                         geom_histogram(binwidth = bin, closed = 'left'))
+                         geom_histogram(aes(y=..count../sum(..count..)), binwidth = bin, closed = 'left'))
   output <- ggplot(data = streets, mapping = aes(x = bearing, weight = length)) + theme_void() +
-    theme(panel.grid.major.y = element_line(size = 1), 
-          plot.title = element_text(size = 30, family = 'Bebas Neue', hjust = .5)) +
-    geom_histogram(binwidth = bin, closed = 'left', fill = 'royalblue4', color = 'black') + 
-    scale_x_continuous(breaks = c(0, 90, 180, 270), limits = c(0 - (bin/2), 360 - (bin/2))) +
+    theme(plot.title = element_text(size = 30, family = 'Bebas Neue', hjust = .5), axis.text.x = element_text(size = 16, family = 'Bebas Neue')) +
+    geom_histogram(aes(y=..count../sum(..count..)), binwidth = bin, closed = 'left', fill = 'royalblue4', color = 'black') + 
+    scale_x_continuous(breaks = c(0, 90, 180, 270), labels = c('N', 'E', 'S', 'W'), limits = c(0 - (bin/2), 360 - (bin/2))) +
     coord_polar(start = 0 - ((bin/2) * pi / 180)) + 
-    scale_y_continuous(breaks = 1, limits = c(-.2 * max(plot[[1]][[1]][[1]]), max(plot[[1]][[1]][[1]]))) + 
-      labs(title = str_split(location, ',')[[1]][1])
+    scale_y_continuous(limits = c(-.2 * max(plot[[1]][[1]][[1]]), max(plot[[1]][[1]][[1]]))) + 
+    geom_hline(yintercept = max(plot[[1]][[1]][[1]]), size = 1) + labs(title = str_split(location, ',')[[1]][1])
 }
 
 get_streets <- function(location_list, bin) {
-  streets <- lapply(location_list, get_streets_ind, bin = bin)
+  streets <- mclapply(location_list, get_streets_ind, bin = bin, mc.cores = 3)
   wrap_plots(streets)
 }
 
-cities <- list('Venice, Italy', 'San Francisco, CA', 'Berlin, Germany', 'Paris, France', 
-             'Osaka, Japan', 'Shanghai, China', 'Rome, Italy', 'Amsterdam, Netherlands', 
-             'London, UK', 'Miami, Florida', 'Toronto, Canada', 'Cairo, Egypt')
+mclapply()
+
+cities <- list('Venice, Italy', 'San Francisco, CA', 'Amsterdam, Netherlands', 'Berlin, Germany', 'Paris, France', 
+            'Osaka, Japan')#, 'Shanghai, China', 'Rome, Italy', 
+             #'London, UK', 'Miami, Florida', 'Toronto, Canada', 'Cairo, Egypt')
 
 fig <- get_streets(cities, 10)
 
